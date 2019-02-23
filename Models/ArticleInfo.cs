18bacce07648fe85228716029d7e053302968f46
github <431557313@qq.com>
2019-02-23 17:56:13 +0800

    + 提交文件

---
+++ Models/ArticleInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+   public class ArticleInfo
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// 博客标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 博客内容
+        /// </summary>
+        public string contents { get; set; }
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string author { get; set; }
+        /// <summary>
+        /// 发布时间
+        /// </summary>
+        public DateTime create_time { get; set; }
+        /// <summary>
+        /// 点赞数
+        /// </summary>
+        public int clickCount { get; set; }
+        /// <summary>
+        /// 封面
+        /// </summary>
+        public string photo { get; set; }
+    }
+}
---
+++ Models/UserInfoNew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Models
+{
+    public class UserInfoNew
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public int Age { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public DateTime create_date { get; set; }
+    }
+}
